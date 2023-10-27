9556705ae8617d86c176dfe2b813cf5ab46953d5
Dnico13 <Dnico13@hotmail.com>
2023-10-28 01:13:06 +0200

    deroulement ok

--- contact.php
+++ contact.php
@@ -4,6 +4,17 @@ require_once './navbar.php';
 ?>
 
 <body>
+<form>
+  <label for="nom">Nom :</label>
+  <input type="text" id="nom" name="nom"><br><br>
+  <label for="prenom">Prénom :</label>
+  <input type="text" id="prenom" name="prenom"><br><br>
+  <label for="email">Adresse e-mail :</label>
+  <input type="email" id="email" name="email"><br><br>
+  <label for="telephone">Numéro de téléphone :</label>
+  <input type="tel" id="telephone" name="telephone"><br><br>
+  <input type="submit" value="Envoyer">
+</form>
 
 
 
