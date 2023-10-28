fafce20b80502ab5a90090c1925f91a8a0b42ecf
Dnico13 <Dnico13@hotmail.com>
2023-10-28 02:03:21 +0200

    charte ok

--- Charte.php
+++ Charte.php
@@ -4,15 +4,23 @@ require_once './navbar.php';
 ?>
 
 <body>
-
-
-
-
-
+    <div class="containerCharte">
+        <img src="./asset/interieur6.jpg" alt="illustration d un salon d un appartement design et moderne" class="SizePicCharte1">
+        <div class="Bullep">
+            <p class="pCharte"><span>Un chantier maîtrisé de A à Z:</span><br><br>
+            Du début à la fin, votre chantier est géré et maîtrisé. Après validation du projet d’aménagement proposé ainsi que du devis. Vous aurez simplement deux interlocuteurs. Je travaille en étroite collaboration avec mon collègue qui a une entreprise générale tout corps d’état. Mon collaborateur met passion et savoir faire dans votre projet
+            .<br><br>
+            <span>Un Chantier tous corps d’état:</span><br>
+            Confiez votre projet à des personnes de confiance.Nous nous chargeons de vous fournir un devis détaillé, précis et correspondant à votre chantier. Votre projet que nous aurons élaboré sera mis en œuvre par mes collaborateurs qui assurent un savoir faire ainsi qu’une compétence de qualité.
+            De la conception au chantier vous n’avez que deux interlocuteurs l’entreprise de construction et moi pour un projet unique et personnalisé.</p>
+        </div>
+        <img src="./asset/interieur7.webp" alt="illustration d un interieur d une salle de bain calme et reposante" class="SizePicCharte2">
+    </div>
 </body>
 <footer>
     <?php
-        require_once './footer.php';
+    require_once './footer.php';
     ?>
 </footer>
+
 </html>
