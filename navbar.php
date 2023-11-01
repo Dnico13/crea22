37e26afe3030987747b9b63138c2b8c670a3eb2d
Dnico13 <Dnico13@hotmail.com>
2023-11-01 23:30:59 +0100

    navbar en rubrique ok

--- navbar.php
+++ navbar.php
@@ -1,39 +1,23 @@
 <header>
   <nav>
     <div class="header">
-      <a href="./accueil.php" ><img src="./asset/crea22_logo-white.webp" class="logo1" alt="logo du cabinet Crea22"></a>
+      <a href="./accueil.php"><img src="./asset/crea22_logo-white.webp" class="logo1" alt="logo du cabinet Crea22"></a>
 
-      <ul class="nav justify-content-end navbar navbar-dark navmobile ">
-        <li class="nav-item">
-          <a class="nav-link disabled" href="./intervention.php">Périmetre d'intervention</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./deroulement.php">Déroulement d'une intervention</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./Charte.php">Charte de confiance</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="./realisation.php">Mes Réalisations</a>
-        </li>
+      <div class="dropdown me-5 mt-4">
+        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+          Rubriques
+        </a>
 
-      </ul>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+          <li><a class="dropdown-item" href="./intervention.php">Périmetre d'intervention</a></li>
+          <li><a class="dropdown-item" href="./deroulement.php">Déroulement d'une intervention</a></li>
+          <li><a class="dropdown-item" href="./Charte.php">Charte de confiance</a></li>
+          <li><a class="dropdown-item" href="./realisation.php">Mes Réalisations</a></li>
+          <li><a class="dropdown-item" href="./espaceClient.php">Espace client</a></li>
+          <li><a class="dropdown-item" href="contact.php">Contactez-moi</a></li>
+        </ul>
+      </div>
     </div>
   </nav>
- <!-- <div class="dropdown navrubrique">
-    <a class="btn btn-dark dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Rubriques
-    </a>
 
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="instruments.html">Instruments</a></li>
-      <li><a class="dropdown-item" href="Dates.html">Evenements</a></li>
-      <li><a class="dropdown-item" href="partenaires.html">Partenaires</a></li>
-      <li><a class="dropdown-item" href="vidéos.html">Video</a></li>
-      <li><a class="dropdown-item" href="boutique.html">Boutique</a></li>
-      <li><a class="dropdown-item" href="espacepro.html">Espace PRO</a></li>
-      <li><a class="dropdown-item" href="contact.html">Contact</a></li>
-    </ul>
-  </div>
-  </div> -->
 </header>
