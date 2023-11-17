22deef17a7ad3bda7cf47091a269d1f1a4d7bb08
Dnico13 <Dnico13@hotmail.com>
2023-11-17 15:53:56 +0100

    detailrealisation  re ok

--- detailRealisation.php
+++ detailRealisation.php
@@ -51,23 +51,23 @@ $idp = $id + 1;
 
                     <div class="h5 nomClient"><?= $DetailetImages[$id]['Titre'] ?>:</div>
                     <p>
-                        <?= $DetailetImages[$id]['Detail1'] ?>
+                        <?= $DetailetImages[$id]['detail1'] ?>
                     </p>
 
                     <div class="h5 programClient">PROGRAMME:</div>
                     <p>
-                        <?= $DetailetImages[$id]['Detail2'] ?>
+                        <?= $DetailetImages[$id]['detail2'] ?>
                     </p>
                     <div class="h5 Materiaux">PLANCHE MATÉRIAUX :</div>
                     <p>
-                        <?= $DetailetImages[$id]['Detail3'] ?>
+                        <?= $DetailetImages[$id]['detail3'] ?>
                     </p>
                 </div>
                 <!--<p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>-->
 
                 <div class="d-grid gap-2 d-md-flex justify-content-md-center justify-content-lg-between">
                     <button type="button" class="btn btn-secondary btn-lg px-4 me-md-2">
-                        <a href="./detailRealisation.php?Id=<?= $idp ?>">
+                        <a href="./detailRealisation.php?Id=<?= $idm ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
                                 <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
                             </svg>
@@ -75,7 +75,7 @@ $idp = $id + 1;
                     </button>
 
                     <button type="button" class="btn btn-secondary btn-lg px-4">
-                        <a href="./detailRealisation.php?Id=<?= $idm ?>">
+                        <a href="./detailRealisation.php?Id=<?= $idp ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
                                 <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
                             </svg>
